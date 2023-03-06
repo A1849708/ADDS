e9dd5ebbfcef7039a257074a17bfe0d2689142e9
A1849708 <mathewalex2004@gmail.com>
2023-03-06 17:53:12 +1030

    10th commit

--- human.cpp
+++ human.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
         Human::Human (){this->humanName="Human";}
         Human::Human(string name){this->humanName=name;}
-        void Human::makeMove(){cout<<"\nEnter in a move: ";cin>>humanMove;}
+        char Human::makeMove(){cout<<"\nEnter in a move: ";cin>>humanMove;}
         char Human::returnMove(){return humanMove;}
         string Human::getName(){return humanName;}
