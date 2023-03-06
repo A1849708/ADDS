3b74ab961e6264799a9819d4d67015c953d18572
A1849708 <mathewalex2004@gmail.com>
2023-03-06 18:09:38 +1030

    1

---
+++ Human.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Human.h"
+
+using namespace std;
+
+
+        Human::Human (){this->humanName="Human";}
+        Human::Human(string name){this->humanName=name;}
+        char Human::makeMove(){cout<<"\nEnter in a move: ";cin>>humanMove;}
+        char Human::returnMove(){return humanMove;}
+        string Human::getName(){return humanName;}
