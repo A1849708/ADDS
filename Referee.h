0572a356522336bc26527db74c96b685e98a4090
A1849708 <mathewalex2004@gmail.com>
2023-03-06 18:13:19 +1030

    1

---
+++ Referee.h
@@ -0,0 +1,14 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+#include "Player.h"
+
+#include <string>
+
+class Referee {
+    private:
+        char move;
+    public:
+        Referee ();
+        Player * refGame(Player * player1, Player * player2);
+};
+#endif
